91a92ffd98b6e68a9e6e43a1c395ec6e8cd03219
sucsilant07 <suc.programing07@gmail.com>
2022-12-23 18:09:18 -0500

    Update

---
+++ Primer-proyecto-Node/primera-lista.js
@@ -0,0 +1,9 @@
+const fecha = new Date(1997, 10, 7);
+const libro = {
+    titulo : "Clean Code",
+    autor : "Robert C. Martin",
+    fecha : new Date (2008, 7, 1),
+    url : "https://www.amazon.es/Clean-Code-Handbook-Software-Craftsmanship/dp/0132350882",
+}
+const lista = ["Sandro", 25, false, fecha, libro];
+console.log(lista)
