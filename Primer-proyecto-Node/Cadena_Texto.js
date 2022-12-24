76a1e1bde67118fc4a0ccfd6213bf4b208e32157
sucsilant07 <suc.programing07@gmail.com>
2022-12-24 02:18:12 -0500

    Update

---
+++ Primer-proyecto-Node/Cadena_Texto.js
@@ -0,0 +1,24 @@
+let str_nombre = "Sandro";
+let str_apellido = "Urbina";
+let estudiante = str_nombre + " " + str_apellido;
+let estudianteMayus= estudiante.toUpperCase();
+let estudianteMinus= estudiante.toLowerCase();
+let cantidad = estudiante.length;
+let primera_letra = str_nombre.charAt(0);
+let ultima_letra = str_apellido.charAt(5);
+let sin_espacios = estudiante.replace(/\s+/g, "");
+let existe = estudiante.includes("Sandro");
+
+//console.log(estudiante);
+
+//console.log(estudianteMayus);
+
+//console.log(estudianteMinus);
+
+//console.log(cantidad);
+
+//console.log(ultima_letra);
+
+//console.log(sin_espacios);
+
+//console.log(existe);
