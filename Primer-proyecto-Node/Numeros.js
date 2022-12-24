2fa65797ea65a83e0c1b01458ba9d68639f156c0
sucsilant07 <suc.programing07@gmail.com>
2022-12-24 14:40:00 -0500

    Update

--- Primer-proyecto-Node/Numeros.js
+++ Primer-proyecto-Node/Numeros.js
@@ -1,5 +1,6 @@
-let altura_cm = 175;
-let altura_m = 1.75;
-let peso = 74;
-let alt_m = altura_m.toFixed(1);
-let peso_dec = peso.toFixed(2);
+const altura_cm = 175;
+const altura_m = 1.75;
+const peso = 74.5;
+const alt_m = Math.ceil(altura_m);
+const peso_dec = Math.floor(peso);
+const Iguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
