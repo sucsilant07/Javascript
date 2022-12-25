1639f553c6c9f9e4873ca032260da3080b17c183
sucsilant07 <suc.programing07@gmail.com>
2022-12-25 17:24:32 -0500

    Update

---
+++ Primer-proyecto-Node/Conjunto.js
@@ -0,0 +1,7 @@
+let familia = ["Sandro", "Mitzi", "Mateo", "Pilsen", "Pancho", "Junior"];
+let familia_set = new Set(familia); 
+
+familia_set.add("Sandro");
+familia_set.add("Javascript");
+
+console.log(familia_set)
